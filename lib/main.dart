9b6c84a4a7591f0834188d6786d72781ee58526a
chokearth <chokearth@gmail.com>
2023-02-19 13:41:17 +0100

    Add Firebase option for dev

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,22 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:memory_ez/envConfig.dart';
+import 'package:memory_ez/firebase_options_dev.dart';
+import 'package:memory_ez/firebase_options_prod.dart';
 import 'package:memory_ez/routes.dart';
 import 'package:memory_ez/theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (EnvConfig.isDev) {
+    await Firebase.initializeApp(
+      options: DevFirebaseOptions.currentPlatform,
+    );
+  } else {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
 
   var firestore = FirebaseFirestore.instance;
   if (EnvConfig.isDev) {
